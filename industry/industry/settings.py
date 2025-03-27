7e4634870bc374929d4697dc46fc72b381203ada
siddarthbhave <siddarthbhave@gmail.com>
2025-03-27 14:41:55 -0700

    added 8000 cors allowed origins

--- industry/industry/settings.py
+++ industry/industry/settings.py
@@ -135,6 +135,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://ec2-44-202-186-76.compute-1.amazonaws.com:3000",
+    "http://ec2-44-202-186-76.compute-1.amazonaws.com:8000",
     "http://localhost:8000",
     "http://localhost:5173",
     "http://localhost:3000",
